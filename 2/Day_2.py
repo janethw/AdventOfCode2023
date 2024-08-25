a3b5e60323294ea12bd89e70a03809e85b1b1b1a
Janet Hamilton-Wilks <165585567+janethw@users.noreply.github.com>
2024-08-25 21:52:25 +0100

    5th commit. Create game_dict to hold game data from input.txt. Parse input text string to get dict key. TODO: Parse string to get dict of dicts for each cube colour

--- 2/Day_2.py
+++ 2/Day_2.py
@@ -1,9 +1,16 @@
 def main():
 
+    # create dict to add data from input.txt
+    game_dict = {}
+
     # read in code from input.txt
     with (open('input.txt') as f):
         for line in f:
             print(line)
+            raw_key, raw_value = line.strip().split(':')
+            key = int(raw_key[5:])
+            print(f"{key=}")
+            game_dict[key] = raw_value
 
 
 if __name__ == "__main__":
