ddba1589531051c5d64f27e213bcfff92958a134
Janet Hamilton-Wilks <165585567+janethw@users.noreply.github.com>
2024-08-25 21:53:53 +0100

    6th commits. Added comments to Day_2.py

--- 2/Day_2.py
+++ 2/Day_2.py
@@ -7,9 +7,13 @@ def main():
     with (open('input.txt') as f):
         for line in f:
             print(line)
+            # Set dict key
             raw_key, raw_value = line.strip().split(':')
             key = int(raw_key[5:])
             print(f"{key=}")
+
+            # Get dict of dicts for the three cube colours
+            print(f"{value=}")
             game_dict[key] = raw_value
 
 
